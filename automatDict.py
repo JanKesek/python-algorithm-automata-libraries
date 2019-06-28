57b565d975d5a2694813d7f8eb986320ad2221f0
JanKesek <jakesek@wp.pl>
2019-06-28 19:20:43 +0200

    NFA to DFA conversion and string matching function

--- automatDict.py
+++ automatDict.py
@@ -3,29 +3,23 @@ DFA = {}
 
 def convert(NFA):
 	for key,value in NFA.items():
-		#print(value)
 		for przejscia in value:
-			#print("COT O KURWA JEST", przejscia[1])
 			if len(przejscia[1])>1:
 				newKey=[''.join(przejscia[1])]
-				#flatList= [item for sublist in DFA.values() for item in sublist]
 				if newKey not in DFA.values():
 					t=[]
 					w=[]
 					w.append(przejscia[0])
 					w.append(newKey)
 					t.append(w)
-					#print("JAPIERDOLE", t)
 					DFA[key]=t
 			else:
-				#flatList= [item for sublist in DFA.values() for item in sublist]
 				if przejscia[1] not in DFA.values():
 					t=[]
 					w=[]
 					w.append(przejscia[0])
 					w.append(przejscia[1])
 					t.append(w)
-					#print("NIEW KURWIAJ MNIE", w)
 					DFA[key]=w
 		print(DFA)
 		print(NFA)
